d7bdee217f78fab59a7f4c57642a55d7c351c418
peterSanyo <peter.sanyo@code.berlin>
2023-10-16 10:50:34 +0200

    Initial Commit

---
+++ SET!/SET_App.swift
@@ -0,0 +1,17 @@
+//
+//  SET_App.swift
+//  SET!
+//
+//  Created by Péter Sanyó on 16.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SET_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
